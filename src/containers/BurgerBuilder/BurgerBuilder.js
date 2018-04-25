6675fde0f0329448fb8c036f325bcb2b1b58c2eb
Anu Shankar <anushankardivakaran@gmail.com>
2018-04-25 10:48:46 +0530

    for updates

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+const ITEM_PRICE = {
+    salad: 0.5,
+    bacon: 0.8,
+    cheese: 1.3,
+    meat: 0.6
 
+}
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
@@ -25,17 +31,33 @@ class BurgerBuilder extends Component {
            ...this.state.ingredients
        }
        updateitem[type] = updatedCount;
+       const priceAddition = ITEM_PRICE[type];
+       const oldPrice  = this.state.totalPrice;
+       const newPrice = oldPrice + priceAddition;
+       this.setState({totalPrice:newPrice,ingredients:updateitem});
 
     }
     deleteItem = (type) =>{
-
+       const oldCount = this.state.ingredients[type];
+       if(oldCount <= 0){
+           return;
+       }
+       const updatedCount = oldCount-1;
+       const updateitem ={
+           ...this.state.ingredients
+       }
+       updateitem[type] = updatedCount;
+       const priceDeduction = ITEM_PRICE[type];
+       const oldPrice  = this.state.totalPrice;
+       const newPrice = oldPrice - priceDeduction;
+       this.setState({totalPrice:newPrice,ingredients:updateitem});
     }
 
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls/>
+                <BuildControls added = {this.addItem} remove ={this.deleteItem}/>
             </Aux>
         );
     }
